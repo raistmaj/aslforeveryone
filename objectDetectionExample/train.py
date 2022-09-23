527c48d1ec68516bb6926ced677fd5d1f195a29e
Jimmy Dong <ruimingdong@microsoft.com>
2022-09-22 18:00:48 -0700

    train with 30 classes

--- objectDetectionExample/train.py
+++ objectDetectionExample/train.py
@@ -26,7 +26,33 @@ labels = [
     {"name": "yes", "id": 1},
     {"name": "no", "id": 2},
     {"name": "thank you", "id": 3},
-    {"name": "hello", "id": 4}
+    {"name": "hello", "id": 4},
+    {"name": "A", "id": 5},
+    {"name": "B", "id": 6},
+    {"name": "C", "id": 7},
+    {"name": "D", "id": 8},
+    {"name": "E", "id": 9},
+    {"name": "F", "id": 10},
+    {"name": "G", "id": 11},
+    {"name": "H", "id": 12},
+    {"name": "I", "id": 13},
+    {"name": "J", "id": 14},
+    {"name": "K", "id": 15},
+    {"name": "L", "id": 16},
+    {"name": "M", "id": 17},
+    {"name": "N", "id": 18},
+    {"name": "O", "id": 19},
+    {"name": "P", "id": 20},
+    {"name": "Q", "id": 21},
+    {"name": "R", "id": 22},
+    {"name": "S", "id": 23},
+    {"name": "T", "id": 24},
+    {"name": "U", "id": 25},
+    {"name": "V", "id": 26},
+    {"name": "W", "id": 27},
+    {"name": "X", "id": 28},
+    {"name": "Y", "id": 29},
+    {"name": "Z", "id": 30},
 ]
 
 with open(os.path.join(ANNOTATION_PATH, 'label_map.pbtxt'), 'w') as f:
@@ -50,7 +76,7 @@ pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
 with tf.io.gfile.GFile(CONFIG_PATH, "r") as f:
     proto_str = f.read()
     text_format.Merge(proto_str, pipeline_config)
-pipeline_config.model.ssd.num_classes = 4
+pipeline_config.model.ssd.num_classes = 30 # change this to the number of classes
 pipeline_config.train_config.batch_size = 4
 pipeline_config.train_config.fine_tune_checkpoint = os.path.join(
     PRETRAINED_MODEL_PATH, 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8', 'checkpoint', 'ckpt-0')
